cf59d570bcd5e5c873287bfa2cce5181a07559b3
Corentin Allemand <corentin.allemand@rakuten.com>
2018-08-12 15:17:25 +0200

    add NETWORK_SCANNER_HOSTS

--- debug/test.js
+++ debug/test.js
@@ -1,16 +1,19 @@
 const sails = require('./mock-sails');
 global.gladys = require('./mock-gladys');
 
-const index = require('../index')(sails);
+//const index = require('../index')(sails);
+const scan = require('../lib/scan')(sails);
+
 
 /*
 index.setup().then(() => {
     console.log('ICI')
 });
 */
-
+/*
 index.init(sails).then(() => {
     //console.log('ICI')
 });
+*/
 
 
